2e88705cc6fba6dd78fa06f1d92e4a57940ad9d1
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-12-10 13:27:34 +0000

    Make victory and defeat subtitles grey

--- src/main/java/dev/emortal/minestom/blocksumo/game/BlockSumoGame.java
+++ src/main/java/dev/emortal/minestom/blocksumo/game/BlockSumoGame.java
@@ -187,12 +187,12 @@ public class BlockSumoGame extends Game {
 
         Title victoryTitle = Title.title(
                 MiniMessage.miniMessage().deserialize("<gradient:#ffc570:gold><bold>VICTORY!</bold></gradient>"),
-                Component.text(GameWinLoseMessages.VICTORY.random()),
+                Component.text(GameWinLoseMessages.VICTORY.random(), NamedTextColor.GRAY),
                 Title.Times.times(Duration.ZERO, Duration.ofSeconds(2), Duration.ofSeconds(4))
         );
         Title defeatTitle = Title.title(
                 MiniMessage.miniMessage().deserialize("<gradient:#ff474e:#ff0d0d><bold>DEFEAT!</bold></gradient>"),
-                Component.text(GameWinLoseMessages.DEFEAT.random()),
+                Component.text(GameWinLoseMessages.DEFEAT.random(), NamedTextColor.GRAY),
                 Title.Times.times(Duration.ZERO, Duration.ofSeconds(2), Duration.ofSeconds(4))
         );
 
